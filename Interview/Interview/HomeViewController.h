eac2f24f688537c12d1c66070c21cca7c3da19a6
wesley.stjohn <wesley.stjohn@mobileforming.com>
2014-10-22 12:46:31 -0700

    added missing HomeViewController files

---
+++ Interview/Interview/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Interview
+//
+//  Created by Wesley St. John on 10/10/14.
+//  Copyright (c) 2014 mobileforming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextField *txtMaxPrice;
+
+@end
+
